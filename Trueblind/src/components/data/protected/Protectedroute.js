976f29ba02668f2da7329798b90eaf72e3a78af3
HannisenGo1 <hannakarlsson69@gmail.com>
2025-03-12 10:59:27 +0100

    away with js, and fixing json

--- Trueblind/src/components/data/protected/Protectedroute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useUserStore } from '../../../storage/storage';
-const ProtectedRoute = ({ children }) => {
-    const user = useUserStore((state) => state.user);
-    return user ? children : <Navigate to="/" replace/>;
-};
-export default ProtectedRoute;
--- Trueblind/vite.config.ts
+++ Trueblind/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,9 @@ export default defineConfig({
       },
     },
   },
-  base: '/Examen/', 
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
